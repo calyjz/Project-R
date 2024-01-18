94b180a4d3141cb8d83dc74092cfb58b8b9388d8
damoncgrant <dcgrant@ualberta.ca>
2024-01-18 13:50:46 -0700

    Created Base + Movement Done

---
+++ MotionGiantProject Base/Assets/Scripts/Player.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Speed;
+    public Rigidbody2D rb2d;
+    private Vector2 movement;
+    private Vector2 smoothMovement;
+    private Vector2 movementSmoothVelocity;
+
+    private float activeMoveSpeed;
+    public float dashSpeed = 15f;
+
+    public float smoothMovementCountdown = 0.12f;
+    public float dashLength = 0.3f;
+    public float dashCooldown = 1f;
+
+    private float dashCounter;
+    private float dashCoolCounter;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        activeMoveSpeed = Speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Movement controls
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        movement.Normalize();
+
+        smoothMovement = Vector2.SmoothDamp(smoothMovement, movement, ref movementSmoothVelocity, smoothMovementCountdown);
+
+        rb2d.velocity = smoothMovement * activeMoveSpeed;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (dashCoolCounter <= 0 && dashCounter <= 0)
+            {
+                activeMoveSpeed = dashSpeed;
+                dashCounter = dashLength;
+            }
+        }
+
+        if (dashCounter > 0)
+        {
+            dashCounter -= Time.deltaTime;
+
+            if (dashCounter <= 0)
+            {
+                activeMoveSpeed = Speed;
+                dashCoolCounter = dashCooldown;
+            }
+        }
+
+        if (dashCoolCounter > 0)
+        {
+            dashCoolCounter -= Time.deltaTime;
+        }
+       
+    }
+}
