de923506e6ef5166d86cc5aa2749d268b9f04d93
Caly Zheng <calyzheng1@gmail.com>
2024-02-06 15:11:52 -0700

    added movement between two rooms

--- MotionGiantProject Base/Assets/Scripts/EnterDoor.cs
+++ MotionGiantProject Base/Assets/Scripts/EnterDoor.cs
@@ -5,12 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class EnterDoor : MonoBehaviour
 {
+	public int nextRoom;
 
 	void OnCollisionEnter2D(Collision2D col)
 	{
 		if (col.gameObject.tag == "Player")
 		{
-			SceneManager.LoadScene("Room2");
+			Debug.Log("Room" + nextRoom.ToString());
+			SceneManager.LoadScene("Room" + nextRoom.ToString());
 
 		}
 	}
