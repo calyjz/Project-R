bee75195ff19a6bc4196e801d33b711b1d740ed7
Caly Zheng <calyzheng1@gmail.com>
2024-02-11 14:23:06 -0700

    Game loop + updated spawn placements

--- MotionGiantProject Base/Assets/Scripts/EnterDoor.cs
+++ MotionGiantProject Base/Assets/Scripts/EnterDoor.cs
@@ -5,14 +5,38 @@ using UnityEngine.SceneManagement;
 
 public class EnterDoor : MonoBehaviour
 {
-	public int nextRoom;
+	public char Door;
+
+	
 
 	void OnCollisionEnter2D(Collision2D col)
 	{
 		if (col.gameObject.tag == "Player")
 		{
-			Debug.Log("Room" + nextRoom.ToString());
-			SceneManager.LoadScene("Room" + nextRoom.ToString());
+			//North = 0, East = 1, South = 2, West = 3
+			int doorDirection = 0;
+
+			//TODO: Figure out a way to automatically get the door the player went through
+			switch (Door)
+            {
+				case 'N':
+					doorDirection = 0;
+					break;
+				case 'E':
+					doorDirection = 1;
+					break;
+				case 'S':
+					doorDirection = 2;
+					break;
+				case 'W':
+					doorDirection = 3;
+					break;
+				default:
+					Debug.Log("Wrong Direction entered");
+					break;
+			}	
+
+			GameController.Instance.loadNextRoom(doorDirection);
 
 		}
 	}
---
+++ MotionGiantProject Base/Assets/Scripts/StartButtonBehavior.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButtonBehavior : MonoBehaviour
+{
+    public void OnButtonPress()
+    {
+        GameController.Instance.StartRun();
+    }
+}
