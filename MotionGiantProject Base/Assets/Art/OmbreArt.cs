108cde97c18068e14588335d2b141c49d6ecc0fa
Yash Jha <breakdownninja@gmail.com>
2024-02-27 20:31:50 -0700

    Fully Implemented EnemyAI

---
+++ MotionGiantProject Base/Assets/Art/OmbreArt.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+
+public class OmbreArt : MonoBehaviour
+{
+    public int textureSize = 512; // Size of the texture
+    public ColorStop[] colorStops; // Array of color stops
+    public Color firstColor;
+
+    public float ombreScale = 1f;
+    [System.Serializable]
+    public struct ColorStop
+    {
+        [Range(0f, 1f)]
+        public float position; // Position of the color stop as a percentage of the radius
+        public Color color; // Color of the stop
+    }
+    // Start is called before the first frame update
+    void DrawTexture()
+    {
+        Texture2D texture = new Texture2D(textureSize, textureSize);
+
+        // Calculate center position
+        Vector2 center = new Vector2(textureSize / 2, textureSize / 2);
+
+        // Loop through each pixel in the texture
+        for (int y = 0; y < textureSize; y++)
+        {
+            for (int x = 0; x < textureSize; x++)
+            {
+                // Calculate the distance from the current pixel to the center
+                float distance = Vector2.Distance(new Vector2(x, y), center) / (textureSize / 2);
+
+                // Determine color based on color stops
+                Color color = firstColor; // Default color
+                for (int i = 1; i < colorStops.Length; i++)
+                {
+                    if (distance <= colorStops[i].position)
+                    {
+                        float scaledPosition = Mathf.Clamp01((distance - colorStops[i - 1].position) / ombreScale + colorStops[i - 1].position);
+                        float t = Mathf.InverseLerp(colorStops[i - 1].position, colorStops[i].position, scaledPosition);
+                        color = Color.Lerp(colorStops[i - 1].color, colorStops[i].color, t);
+                        break;
+                    }
+                }
+
+                // Set the color of the current pixel in the texture
+                texture.SetPixel(x, y, color);
+            }
+        }
+
+        // Apply changes and assign the texture to a material
+        texture.Apply();
+        //GetComponent<Renderer>().material.mainTexture = texture;
+        GetComponent<Renderer>().sharedMaterial.mainTexture = texture;
+    }
+    void Awake()
+    {
+        DrawTexture();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Create a new texture
+        
+        // Useful in Edit Mode
+        DrawTexture();
+        //GetComponent<Renderer>().material.mainTexture = texture;
+    }
+}
--- MotionGiantProject Base/Assets/Enemy.cs
+++ MotionGiantProject Base/Assets/Enemy.cs
@@ -12,24 +12,147 @@ public class Enemy : MonoBehaviour
     public float attackTime;
     public float startTimeAttack;
 
+    private string mode = "patrol";
+    public float Speed = 2f;
     Transform playerPos;
+
+    private Vector3 directionVector;
+    private int nextPointIndex;
+    private Vector3 nextPointPos;
+    private int reversed = 1;
+
+    public List<Vector3> path;
     void Start()
     {
         spawner = transform.Find("spawner");
         playerPos = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
+
+    void FollowPlayer()
+    {
+        transform.position += (playerPos.position - transform.position).normalized * Time.deltaTime * Speed;
+    }
+    void attackPlayer()
+    {
+        if (attackTime <= 0)
+        {
+            Instantiate(shoot, spawner.position, Quaternion.identity);
+            attackTime = startTimeAttack;
+        }
+    }
+    bool isPlayerInAttackRange()
+    {
+        if (Vector3.Distance(playerPos.position, transform.position) < attackRange && isPlayerInCleanSight())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void setPatrolPath()
+    {
+        int closest = -1;
+        float closestDistance = 9999f;
+        int walls = LayerMask.GetMask("Obstacle");
+
+        for (int index  = 0; index < path.Count; index += 1)
+        {
+
+            if (Vector3.Distance(path[index], transform.position) < closestDistance && Physics2D.Linecast(path[index], transform.position, walls).collider==null)
+            {
+                closest = index;
+                closestDistance = (Vector3.Distance(path[index], transform.position));
+            }
+        }
+        Debug.Log(closest); ;
+        if (closest == -1)
+        {
+            directionVector = new Vector3(0, 0, 0);
+            nextPointIndex = -1;
+            return;
+        }
+        nextPointIndex = closest;
+        nextPointPos = path[closest];
+        directionVector = (nextPointPos - transform.position).normalized;
+    }
+
+    void keepPatrolling()
+    {
+        //Debug.Log(nextPointIndex);
+        if (nextPointIndex == -1)
+        {
+            transform.position += directionVector * Time.deltaTime * Speed;
+            return;
+        }
+        if ((nextPointPos - transform.position).magnitude < 0.1f)
+        {
+            transform.position = nextPointPos;
+            nextPointIndex += reversed;
+            if (nextPointIndex >= path.Count || nextPointIndex <= 0)
+            {
+                reversed *= -1;
+            }
+
+            nextPointPos = path[nextPointIndex];
+            directionVector = (nextPointPos - transform.position).normalized;
+            //float angle = Mathf.Atan2(directionVector.x, -directionVector.y) * Mathf.Rad2Deg;
+            //transform.eulerAngles = new Vector3(0, 0, angle);
+
+            //Debug.Log(directionVector);
+        }
+        transform.position += directionVector * Time.deltaTime * Speed;
+
+    }
+    bool isPlayerInCleanSight()
+    {
+        int walls = LayerMask.GetMask("Obstacle");
+
+        RaycastHit2D hit = Physics2D.Linecast(transform.position, playerPos.position, walls);
+        //Debug.Log(results);
+        //Debug.Log(hit.collider);
+        if (hit.collider!=null)
+        {
+            return false;
+        }
+        return true;
+    }
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(playerPos.position, transform.position) < attackRange)
+        // There are 3 enemy models
+        // ..Patroll
+        // ..Follow Player (When Visible)
+        // ..Follow Player till wall reached
+        //isPlayerInCleanSight();
+        //return;
+
+
+        bool isPlayerAttackable = isPlayerInAttackRange();
+        bool isPlayerVisible = isPlayerInCleanSight();
+        //Debug.Log((isPlayerAttackable, isPlayerVisible));
+        if (isPlayerAttackable)
         {
-            if(attackTime <= 0)
-            {
-                Instantiate(shoot, spawner.position, Quaternion.identity);
-                attackTime = startTimeAttack;
-            }
+            mode = "attack";
+            attackPlayer();
+        } else if (isPlayerVisible)
+        {
+            mode = "follow";
+            FollowPlayer();
+        } else if (mode == "patrol")
+        {
+            keepPatrolling();
         }
+        else
+        {
+            mode = "patrol";
+            setPatrolPath();
+        }
+        
+        
 
         if (attackTime > 0)
         {
