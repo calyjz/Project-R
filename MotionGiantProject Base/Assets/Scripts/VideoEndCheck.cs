ad7c1ed52baefe9741cf331e9414f7146f3c0863
Damon Grant <dcgrant@ualberta.ca>
2024-04-20 03:40:53 -0600

    cutscenese fixed

---
+++ MotionGiantProject Base/Assets/Scripts/VideoEndCheck.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class VideoEndCheck : MonoBehaviour
+{
+    [SerializeField] public VideoPlayer videoPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer.loopPointReached += nextScene;
+    }
+
+    public void nextScene(VideoPlayer vp)
+    {
+        if (SceneManager.GetActiveScene().name == "Intro")
+        {
+            GameController.Instance.UpdateGameState(GameState.Run);
+        } else if (SceneManager.GetActiveScene().name == "Outro")
+        {
+            GameController.Instance.GameReset();
+        }
+    }
+}
