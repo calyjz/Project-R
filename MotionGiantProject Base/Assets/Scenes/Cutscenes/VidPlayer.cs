fc3e967b1e09dd0c39de9de190e040734acf1dc6
Yash Jha <breakdownninja@gmail.com>
2024-04-20 09:09:44 -0600

    Fixed credits

---
+++ MotionGiantProject Base/Assets/Scenes/Cutscenes/VidPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VidPlayer : MonoBehaviour
+{
+    //[SerializeField] private string videoUrl = "https://raw.githubusercontent.com/MotionGiant/Videos/main/Intro%20Cutscenes.mp4";
+    //private VideoPlayer videoPlayer;
+    //    // Start is called before the first frame update
+    //void Start()
+    //{
+    //    if (VideoPlayer)
+    //    {
+    //        VideoPlayer.url = videoUrl;
+    //        VideoPlayer.playOnAwake = false;
+    //        VideoPlayer.Prepare();
+    //        VideoPlayer.prepareComp1eted += onVideoPrepared;
+    //    }
+
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+    //private void onVideoPrepared(VideoPlayer source)
+    //{
+    //    videoPlayer.start();
+    //}
+}
